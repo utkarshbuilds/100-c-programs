85d0527493e290fa21320c54957d75eba1721853
Utkarsh Raj <utkarshraj1825@gmail.com>
2026-03-16 17:40:56 +0530

    Create 02_intro.c

---
+++ src/01_basics/02_intro.c
@@ -0,0 +1,19 @@
+/*
+Program: Introdution
+Discription: Print a short introduction on screen
+Author: Utkarsh Raj
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int age = 19;
+  char *name = "Utkarsh Raj";
+  char *city = "Baraut(Bagphat)";
+
+  printf("My name is %s.\nI am %d year old.\nI am from %s.\n", name, age, city);
+
+  return 0;
+}
+
