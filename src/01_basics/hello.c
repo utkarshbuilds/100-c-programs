0a8e3ec973ae971b1c49474ac927c84bcddb0a66
Utkarsh Raj <utkarshraj1825@gmail.com>
2026-03-16 17:32:55 +0530

    created hello.c with comments and fix output format
    Added comments for program details and corrected print statement.

---
+++ src/01_basics/hello.c
@@ -0,0 +1,13 @@
+/* 
+  Program: Hello World
+  Discription: Print "Hello, World" on the screen
+  Author: Utkarsh Raj
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello, World\n");
+  return 0;
+}
