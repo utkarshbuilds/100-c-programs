1fd1bbe5c1c4de87eebd84fedb5f96be8ac17558
Utkarsh Raj <utkarshraj1825@gmail.com>
2026-03-16 17:13:22 +0530

    Add sum calculation program in sum.c
    Implement a simple program to calculate the sum of two integers.

---
+++ sum.c
@@ -0,0 +1,18 @@
+#inlcude <stdio.h>
+
+int main()
+{
+  //declaring the variables
+  int a, b, sum=0;
+
+  // taking input from user
+  scanf("%d %d", &a, &b);
+
+  // calculating the sum
+  sum = a + b;
+
+  // printing the sum 
+  printf("%d", sum);
+
+  return 0;
+}
