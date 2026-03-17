101f18f449cfd13e1eed8a6a43d23c8dee95c898
Utkarsh Raj <utkarshraj1825@gmail.com>
2026-03-17 08:44:27 +0530

    Refactor Celsius to Fahrenheit conversion program
    Updated the Celsius to Fahrenheit converter for better readability and error handling.

--- src/01_basics/07_convert_celsius_to_fahrenheit.c
+++ src/01_basics/07_convert_celsius_to_fahrenheit.c
@@ -1,25 +1,40 @@
 /*
-Program: Convert Temperature from Celsius to Fahrenheit.
+Program: Celsius to Fahrenheit Converter.
 Description: Takes temperature in Celsius, converts it to Fahrenheit, and prints the result.
+Date: 17-Mar-2026
+Input: Temperature in Celsius(float)
+Output: Temprature in Fahrenheit(float)
+Version: 1.0
 Author: Utkarsh Raj
 */
 
 #include <stdio.h>
 
+// -------- Conversion Ratio --------
+#define RATIO (9.0 / 5.0)
+
 int main()
 {
   // Variable Declaration
-  float celsius, fahrenheit;
+  float celsius
+  float fahrenheit;
 
   // -------- Input Section --------
-  printf("Enter temperature value: ");
-  scanf("%f", &celsius);
+  printf("Temperature Converter\n");
+  printf("-------------------------------\n");
+  printf("Enter temperature in Celsius(°C): ");
+
+  if(scanf("%f", &celsius) != 1)
+  {
+      printf("Error\n");
+      return 1;
+  }
 
   // -------- Processing Section --------
-  fahrenheit = (1.8 * celsius) + 32;
+  fahrenheit = (RATIO * celsius) + 32;
 
   // -------- Output Section --------
-  printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
+  printf("Temperature in Fahrenheit: %.2f°F\n", fahrenheit);
 
   return 0;
 }
