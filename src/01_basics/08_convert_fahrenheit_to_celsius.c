dc539571d667547169812529916e49d4588f4518
Utkarsh Raj <utkarshraj1825@gmail.com>
2026-03-20 08:45:36 +0530

    Update 08_convert_fahrenheit_to_celsius.c

--- src/01_basics/08_convert_fahrenheit_to_celsius.c
+++ src/01_basics/08_convert_fahrenheit_to_celsius.c
@@ -2,9 +2,6 @@
 Program: Fahrenheit to Celsius Converter.
 Description: Takes temperature in Fahrenheit, converts it to Celsius, and prints the result.
 Date: 17-Mar-2026
-Input: Temperature in Fahrenheit(float)
-Output: Temperature in Celsius(float)
-Version: 1.0
 Author: Utkarsh Raj
 */
 
